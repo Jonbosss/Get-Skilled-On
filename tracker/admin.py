f8f546e2efd57b3c082c968a76caf6e5944323aa
Jonbosss <jonathanfarhat@gmail.com>
2026-01-16 15:56:53 -0800

    Added profile page for front end

--- tracker/admin.py
+++ tracker/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from .models import Profile, Activity
 
 admin.site.register(Profile)
-admin.site.register(Activity)
+
+
+class ActivityAdmin(admin.ModelAdmin):
+    # This tells Django which columns to show in the list view
+    list_display = ('title', 'user', 'category', 'xp_amount', 'date_completed')
+
+    # This adds a filter sidebar so you can sort by category
+    list_filter = ('category', 'user')
+
+
+# Register the new settings
+admin.site.register(Activity, ActivityAdmin)
--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Profile
 
-# Create your views here.
+
+def profile_view(request):
+    # This finds the stats for the person currently logged in
+    profile = Profile.objects.get(user=request.user)
+
+    # This sends those stats to the webpage template
+    return render(request, 'tracker/profile.html', {'profile': profile})
